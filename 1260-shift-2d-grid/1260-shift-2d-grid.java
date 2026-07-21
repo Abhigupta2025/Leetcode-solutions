65cd2525d2efdb4856a35ff926cd046dc367f18c
Abhishek Gupta <abhishekgupta29072005@gmail.com>
2026-07-21 18:04:49 +0530

    Time: 6 ms (55.28%), Space: 47.3 MB (25.46%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -2,36 +2,23 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-
-        int total = m * n;
-        k %= total;
-
-        List<List<Integer>> ans = new ArrayList<>();
-
+        List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < n; j++)
-                row.add(0);
-            ans.add(row);
-        }
-
-        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                row.add(0);
+            }
+            res.add(row);
+        }
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                int val = ((r * n + c) + k) % (m * n);
+                int rc = val / n;
+                int cl = val % n;
 
-                // index in 1D array (before rotation)
-                int oldIndex = i * n + j;
-
-                // index in 1D array (after rotation)
-                int newIndex = (oldIndex + k) % total;
-
-                // changing from 1D back to 2D
-                int newRow = newIndex / n;
-                int newCol = newIndex % n;
-
-                ans.get(newRow).set(newCol, grid[i][j]);
+                res.get(rc).set(cl, grid[r][c]);
             }
         }
-
-        return ans;
+        return res;
     }
 }
